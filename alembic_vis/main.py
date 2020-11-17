c4399b05f3a5c4028c8874b9a0d4fa24119e797b
arsen <c4ptain.bld@yandex.ru>
2020-11-17 23:27:36 +0300

    initial

---
+++ alembic_vis/main.py
@@ -0,0 +1,68 @@
+import re
+import sys
+import select
+from collections import namedtuple
+from typing import List
+
+from graphviz import Digraph
+
+
+class AlembicHistoryAnalyzer:
+    regexp = re.compile(r'(?P<from_node>.+) -> (?P<to_node>[\w\d]+)( |, )(\(.*\), )?(?P<comment>.*)')
+    
+    HistoryEntry = namedtuple('HistoryEntry', ('from_node', 'to_node', 'comment'))
+    Node = namedtuple('Node', ('name', 'comment'))
+    Edge = namedtuple('Edge', ('from_node', 'to_node'))
+    
+    @classmethod
+    def make_nodes(cls, history: List[HistoryEntry]):
+        nodes = set()
+        for entry in history:
+            for from_node in entry.from_node.split(','):
+                node_name = from_node.strip()
+                nodes.add(cls.Node(from_node.strip(), f'{node_name} ({entry.comment})'))
+            node_name = entry.to_node.strip()
+            nodes.add(cls.Node(entry.to_node.strip(), f'{node_name} ({entry.comment})'))
+        
+        return nodes
+    
+    @classmethod
+    def make_edges(cls, history: List[HistoryEntry]):
+        edges = []
+        for entry in history:
+            edges.extend(
+                [cls.Edge(from_node.strip(), entry.to_node.strip()) for from_node in entry.from_node.split(',')])
+        
+        return edges
+    
+    @classmethod
+    def show_graph(cls, alembic_history):
+        history = []
+        for match in cls.regexp.finditer(alembic_history):
+            groups = match.groupdict()
+            history.append(cls.HistoryEntry(groups['from_node'], groups['to_node'], groups['comment']))
+        
+        nodes = cls.make_nodes(history)
+        edges = cls.make_edges(history)
+        
+        dot = Digraph(comment='Alembic migrations')
+        for node in nodes:
+            dot.node(node.name, node.comment)
+        
+        dot.edges([(edge.from_node, edge.to_node) for edge in edges])
+        dot.render('migrations.gv', view=True, cleanup=True)
+
+
+def run():
+    i, o, e = select.select([sys.stdin], [], [], 1)
+    if i:
+        alembic_history = sys.stdin.read()
+    else:
+        print('No input')
+        exit(1)
+    
+    AlembicHistoryAnalyzer.show_graph(alembic_history)
+
+
+if __name__ == '__main__':
+    run()
---
+++ setup.py
@@ -0,0 +1,19 @@
+from os import path
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='alembic-vis',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    long_description=open(path.join(path.dirname(__file__), 'README.md')).read(),
+    install_requires=['graphviz'],
+    python_requires='>=3.7',
+    entry_points={
+        'console_scripts': [
+            'alembic_vis = alembic_vis.main:entry_point',
+        ],
+    },
+)
